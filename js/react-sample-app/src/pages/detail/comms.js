f52b8a1d116314d9234b4031183aeb0a3361c7d6
todanaoki-study <toda.naoki3131@gmail.com>
2025-11-03 10:55:31 +0900

    全体ページ遷移の続き

--- js/react-sample-app/src/pages/detail/comms.js
+++ js/react-sample-app/src/pages/detail/comms.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Btn from "../../component/btn";
 
 function Comms() {
     return (
@@ -8,6 +9,15 @@ function Comms() {
                 <h2 className="comms__attention">相手のメッセージ待ち
                     <br></br>
                     相手からの返信をお待ちください。</h2>
+
+                <p className="comms__explanation">
+                    もし相手とのやり取りを通して、相手が貸出を認証した場合、以下のような決済URLが送信されます。
+                </p>
+
+                <article>
+                    <p>以下のボタンから決済と予約を確定してください。</p>
+                    <Btn text="決済" to="/settle"></Btn>
+                </article>
             </div>
         </div>
     );
